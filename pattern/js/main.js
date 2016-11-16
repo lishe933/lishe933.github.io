4f6e9fb017ce2730beeb6ba6d3ae8a0a01c316e1
Eric Lish <lishe933@newschool.edu>
2016-11-16 17:10:05 +0000

    Adding index file

---
+++ pattern/js/main.js
@@ -0,0 +1,79 @@
+$("body").css
+("background-color", "blue");
+
+for (var i=0; i < 1; i++) { 
+
+$("body").append("<div class = 'dot'></div>");
+
+}
+
+var mouseeneter = false;
+
+$(".dot").mouseover(function(){
+    $(".dot").css("opacity", "0"); 
+        
+  
+    });
+    
+    $(".dot").mouseleave(function() {
+   $(".dot").css("opacity", "1");   
+     
+   
+    });
+    
+    $(".dot").mouseover(function(){
+    $(".dot2").css("opacity", "0"); 
+        
+  
+    });
+    
+    $(".dot2").mouseleave(function() {
+   $(".dot2").css("opacity", "1");   
+      
+    });
+    
+    $(".dot3").mouseover(function(){
+    $(".dot3").css("opacity", "0"); 
+        
+  
+    });
+    
+    $(".dot3").mouseleave(function() {
+   $(".dot3").css("opacity", "1");   
+     
+   
+    });
+    
+    $(".dot4").mouseover(function(){
+    $(".dot4").css("opacity", "0"); 
+        
+  
+    });
+    
+    $(".dot4").mouseleave(function() {
+   $(".dot4").css("opacity", "1");   
+      
+    });
+    
+    $(".dot5").mouseover(function(){
+    $(".dot5").css("opacity", "0"); 
+        
+  
+    });
+    
+    $(".dot5").mouseleave(function() {
+   $(".dot5").css("opacity", "1");   
+     
+   
+    });
+    
+    $(".dot6").mouseover(function(){
+    $(".dot6").css("opacity", "0"); 
+        
+  
+    });
+    
+    $(".dot6").mouseleave(function() {
+   $(".dot6").css("opacity", "1");   
+      
+    });
